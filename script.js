42c35bd201464af6a566022f40f0a34d26267a8e
Rodrigoclc <rodrigogtn2010@gmail.com>
2023-10-16 10:06:33 -0300

    estrutura e estilo
    últimos toques na estrutura e estilo

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ var adicionarDespesas = document.getElementById('adicionarDespesas')
 var confirmarDespesas = document.getElementById('confirmarDespesas')
 confirmarDespesas.addEventListener('click', okDespesas)
 
+const listadeProjetos = document.getElementById('projetos')
+var projeto01SaldoInicial = [0]
+var projeto01Renda = [0]
+var projeto01Despesas = [0]
+var projeto01SaldoFinal = [0]
+
+listadeProjetos.onchange = function () {
+    
+}
