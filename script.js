3be3394caf1ef895d1c1c97614672217a71d1fff
Rodrigoclc <rodrigogtn2010@gmail.com>
2023-10-30 18:52:18 -0300

    projeto1 funcionando

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-var adicionarDespesas = document.getElementById('adicionarDespesas')
-var confirmarDespesas = document.getElementById('confirmarDespesas')
-confirmarDespesas.addEventListener('click', okDespesas)
+var listaProjetos = document.getElementById('lista-projetos')
+var resultadoSaldoInicial = document.getElementById('resultadoSaldoInicial')
+var resultadoRenda = document.getElementById('resultadoRenda')
+var resultadoDespesa = document.getElementById('resultadoDespesa')
+var resultadoSaldoFinal = document.getElementById('resultadoSaldoFinal')
+var inputAdicionarRenda = document.getElementById('inputAdicionarRenda')
+var confirmarAddRenda = document.getElementById('confirmarAddRenda')
 
-const listadeProjetos = document.getElementById('projetos')
-var projeto01SaldoInicial = [0]
-var projeto01Renda = [0]
-var projeto01Despesas = [0]
-var projeto01SaldoFinal = [0]
+var saldoInicialProjeto1 = 0
+var rendaProjeto1 = 0
+var despesaProjeto1 = 0
+var resultadoFinalProjeto1 = 0
 
-listadeProjetos.onchange = function () {
-    
+
+
+listaProjetos.onchange = function() {
+    const seletor = document.getElementById('lista-projetos')
+    const opcaoSelecionada = seletor.value
+    if (opcaoSelecionada === 'projeto1') {
+        rendaProjeto1 += Number(localStorage.getItem('renda'))
+        despesaProjeto1 += Number(localStorage.getItem('despesa'))
+        resultadoFinalProjeto1 = rendaProjeto1 - despesaProjeto1
+
+        resultadoSaldoInicial.innerHTML = saldoInicialProjeto1.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
+        resultadoRenda.innerHTML = rendaProjeto1.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
+        resultadoDespesa.innerHTML = despesaProjeto1.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
+        resultadoSaldoFinal.innerHTML = resultadoFinalProjeto1.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
+        
+    } else if (opcaoSelecionada === 'projeto2') {
+        alert('Opcao2')
+    } else if (opcaoSelecionada === 'projeto3') {
+        alert('Opcao3')
+    }
 }
