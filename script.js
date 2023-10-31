2b547edacf909ece53c4a40a93eaf8e8e0b36e92
Rodrigoclc <rodrigogtn2010@gmail.com>
2023-10-31 18:47:31 -0300

    focus e value

--- script.js
+++ script.js
@@ -7,27 +7,35 @@ var inputAdicionarRenda = document.getElementById('inputAdicionarRenda')
 var confirmarAddRenda = document.getElementById('confirmarAddRenda')
 
 var saldoInicialProjeto1 = 0
-var rendaProjeto1 = 0
-var despesaProjeto1 = 0
-var resultadoFinalProjeto1 = 0
+var saldoInicialProjeto2 = 0
 
 
 
 listaProjetos.onchange = function() {
     const seletor = document.getElementById('lista-projetos')
     const opcaoSelecionada = seletor.value
+    // Projeto 01
     if (opcaoSelecionada === 'projeto1') {
-        rendaProjeto1 += Number(localStorage.getItem('renda'))
-        despesaProjeto1 += Number(localStorage.getItem('despesa'))
-        resultadoFinalProjeto1 = rendaProjeto1 - despesaProjeto1
+        
+        var rendaProjeto1 = Number(localStorage.getItem('renda1'))
+        var despesaProjeto1 = Number(localStorage.getItem('despesa1'))
+        var resultadoFinalProjeto1 = rendaProjeto1 - despesaProjeto1
 
         resultadoSaldoInicial.innerHTML = saldoInicialProjeto1.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
         resultadoRenda.innerHTML = rendaProjeto1.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
         resultadoDespesa.innerHTML = despesaProjeto1.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
         resultadoSaldoFinal.innerHTML = resultadoFinalProjeto1.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
         
+        //Projeto 02
     } else if (opcaoSelecionada === 'projeto2') {
-        alert('Opcao2')
+        var rendaProjeto2 = Number(localStorage.getItem('renda2'))
+        var despesaProjeto2 = Number(localStorage.getItem('despesa2'))
+        var resultadoFinalProjeto2 = rendaProjeto2 - despesaProjeto2
+
+        resultadoSaldoInicial.innerHTML = saldoInicialProjeto2.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
+        resultadoRenda.innerHTML = rendaProjeto2.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
+        resultadoDespesa.innerHTML = despesaProjeto2.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
+        resultadoSaldoFinal.innerHTML = resultadoFinalProjeto2.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
     } else if (opcaoSelecionada === 'projeto3') {
         alert('Opcao3')
     }
